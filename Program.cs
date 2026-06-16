54c4f7017396370c114eea40af53fbcc26e19386
Khaled Hassan <kh.im.ha218@gmail.com>
2026-06-16 13:29:42 +0300

    adding a couple of secrets

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Text.RegularExpressions;
+using System.Linq;
+
+//string API_KEY= "My_API_KEY";
+
+//string DB_ConnectionString = "Server=localhost;Database=mydb;User Id=myuser;Password=mypassword;";
+
+//var segments="/hi/hello//hihi".Split('/',StringSplitOptions.RemoveEmptyEntries);
+//for(int i=0; i<segments.Length; i++)
+//{
+//    Console.WriteLine(segments[i]);
+//}
+string s = "Aaaaaaaaaaaaaaaaarhrhrh";
+Console.WriteLine(string.Join(", ", Regex.Matches(s, @"(?i)a*").Select(m=>m.Value)));
