99a8136e36d2fd7049607599a2bf10b71cb54597
Khaled Hassan <kh.im.ha218@gmail.com>
2026-06-16 14:30:28 +0300

    Add GitHub token and AWS key to Program.cs
    Added GitHub token and AWS key with comments on formatting issues.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using System.Linq;
 string API_KEY = "My_API_KEY";
 
 string DB_ConnectionString = "Server=localhost;Database=mydb;User Id=myuser;Password=mypassword;";
+string githubToken = "ghp-aBcDeFgHiJkLmNoPqRsTuVwXyZ123456Ab"; // dash instead of underscore
+string awsKey = "AKIAIOSFODNN7EXAMPLEKEY!"; // invalid character at the end
+
 
 var segments = "/hi/hello//hihi".Split('/', StringSplitOptions.RemoveEmptyEntries);
 for (int i = 0; i < segments.Length; i++)
