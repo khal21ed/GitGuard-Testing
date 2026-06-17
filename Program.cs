ca916b4ae54e9a760af32487b4733e5063e36975
Khaled Hassan <kh.im.ha218@gmail.com>
2026-06-17 17:18:24 +0300

    Remove sensitive credentials from Program.cs
    Removed sensitive information such as API keys and connection strings from the code.

--- Program.cs
+++ Program.cs
@@ -5,18 +5,3 @@ using System.Linq;
 
 string API_KEY = "My_API_KEY";
 
-string DB_ConnectionString = "Server=localhost;Database=mydb;User Id=myuser;Password=mypassword;";
-string githubToken = "ghp-aBcDeFgHiJkLmNoPqRsTuVwXyZ123456Ab"; // dash instead of underscore
-string awsKey = "AKIAIOSFODNN7EXAMPLEKEY!"; // invalid character at the end
-
-
-string awsKey = "AKIAIOSFODNN7EXAMPLEKEY1";
-string githubToken = "ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ123456Ab";
-string googleApiKey = "AIzaSyD9tSrke72I6hJ9gHmFQpTpms3K9sQmNc";
-string stripeKey = "sk_live_51HqLmNoPqRsTuVwXyZ123456abcdef";
-string sendgridKey = "SG.aBcDeFgHiJkLmNoPqRsTuV.wXyZ123456abcdefghijklmnopqrstuvwxyz12345";
-string jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyMTIzIn0.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-string dbConnection = "Server=localhost;Database=mydb;User Id=admin;Password=Sup3rS3cr3tP@ssw0rd;";
-string mongoConnection = "mongodb+srv://adminuser:Sup3rS3cr3tP@ssw0rd@cluster0.mongodb.net";
-string privateKey = "-----BEGIN RSA PRIVATE KEY-----";
-string apiKey = "api_key=xK9mP2vL5nQ8rT3sY7wB4hF6jD1cA0eG";
